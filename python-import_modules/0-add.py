9eb42afb07ca27cc3da26c12da03bab0c950f8ef
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-10 21:32:04 -0700

    fixing task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# write  a program that imports the function def add(a, b): from the file
-# add_0.py and prints the result of the addition 1 + 2 = 3
 from add_0 import add
 
-__name__ == '__main__'
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    import sys
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
