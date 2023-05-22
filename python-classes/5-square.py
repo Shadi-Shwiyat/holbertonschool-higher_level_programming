41aba49726f0a75b2f02e1ead7989de8e2090aba
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-21 18:28:05 -0700

    completed task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Task 4 adds access and update private attributes"""
+
+
+class Square:
+    """Define the square class"""
+    def __init__(self, size=0):
+        """Initialize the square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Prints the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return None
+
+        for i in range(1, self.area() + 1):
+            print('#', end='')
+
+            if i % self.__size == 0 and i > 0:
+                print()
