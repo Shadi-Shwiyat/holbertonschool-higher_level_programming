191f7ae0da6c1e8c92090cb1c4e2eb751c58f94b
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-21 17:23:54 -0700

    adding comments for task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Task 0 is to create an empty class "Square" """
+
+
 class Square:
     """Define the square class"""
     def __init__(self, size=None):
+        """Initialize the square class to none"""
         self.size = size
