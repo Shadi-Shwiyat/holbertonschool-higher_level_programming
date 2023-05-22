0ec1bfc3c9e0a8aa4236fbbf60ce3aefcdf6437e
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-21 18:16:13 -0700

    fixing output error task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         if type(size) != int:
             raise TypeError("size must be integer")
         if size < 0:
-            raise ValueError("size must be greater than 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
         
         def area(self):
