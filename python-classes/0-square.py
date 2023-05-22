aac4ea91704cfa510e8cb23a9f45a91ad0f63042
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-21 17:20:13 -0700

    fixing task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, height="0", width="0"):
-        self.height = height
-        self.width = width
+    """Define the square class"""
+    def __init__(self, size=None):
+        self.size = size
