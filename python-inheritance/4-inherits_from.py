ecc45de02a6dbd31f5c06a3a3ca2661b767da8da
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-30 20:53:00 -0700

    fixing task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-       issubclass(a_class, obj.__class__) is False:
-        return True
+""" 4-inherits_from Module """
 
-    return False
+
+def inherits_from(obj, a_class):
+    """
+    Returns `True` if the object is an instance of a
+    class that inherited (directly or indirectly)
+    from the specified class
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
