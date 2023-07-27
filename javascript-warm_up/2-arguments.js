05bbca85d891cdc648a7ffd07b879c8cb5a516a4
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-27 12:47:57 -0700

    Script prints whether an argument was passed to the program

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
-  console.log('Arguement found');
+  console.log('Argument found');
 } else {
-  console.log('Arguements found');
+  console.log('Arguments found');
 }
