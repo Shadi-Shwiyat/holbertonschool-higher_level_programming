7851dcd882e9dfff60796b998f5d55675bea8724
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-28 15:36:21 -0700

    Function returns the factorial of a given number recursively

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,34 +1,19 @@
 #!/usr/bin/node
 
-function bubbleSort (array) {
-  const n = array.length;
-
-  for (let i = 0; i < n - 1; i++) {
-    // Each iteration will move the largest element to the end
-    for (let j = 0; j < n - i - 1; j++) {
-      // Compare adjacent elements and swap them if they are in the wrong order
-      if (array[j] < array[j + 1]) {
-        // Swap elements using a temporary variable
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return 1;
   }
-  return array;
+  return a * factorial(a - 1);
 }
 
 const args = process.argv.slice(2);
-const unsortedArray = [];
+const num1 = Number(args[0]);
+let retVal;
 
-if (args[0] === undefined || args[1] === undefined) {
-  console.log('0');
+if (args[0] === undefined || isNaN(num1)) {
+  console.log('1');
 } else {
-  let i = 0;
-  while (args[i] !== undefined) {
-    unsortedArray.push(Number(args[i]));
-    i++;
-  }
-  const sortedArray = bubbleSort(unsortedArray);
-  console.log(sortedArray[1]);
+  retVal = factorial(num1);
+  console.log(retVal);
 }
