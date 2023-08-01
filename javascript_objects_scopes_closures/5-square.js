c8b2b0ea1f1e53bab9d22c91e11433211c0eade3
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-01 16:48:32 -0700

    fixed task 5 and added instance method charPrint(c) for task 6

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,49 +1,12 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
-      // create empty object instance if any above conditions are met
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    let row = 0;
-    let column;
-
-    while (row < this.height) {
-      let rowString = '';
-      column = 0;
-
-      while (column < this.width) {
-        rowString += 'X';
-        column++;
-      }
-      console.log(rowString);
-      row++;
-    }
-  }
-
-  rotate () {
-    const tempValue = this.height;
-    this.height = this.width;
-    this.width = tempValue;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
+    this.size = size;
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,50 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
-      // create empty object instance if any above conditions are met
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const extSquare = require('./5-square');
 
-  print () {
-    let row = 0;
-    let column;
-
-    while (row < this.height) {
-      let rowString = '';
-      column = 0;
-
-      while (column < this.width) {
-        rowString += 'X';
-        column++;
-      }
-      console.log(rowString);
-      row++;
-    }
-  }
-
-  rotate () {
-    const tempValue = this.height;
-    this.height = this.width;
-    this.width = tempValue;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+class Square extends extSquare {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
@@ -66,5 +24,4 @@ class Square extends Rectangle {
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
