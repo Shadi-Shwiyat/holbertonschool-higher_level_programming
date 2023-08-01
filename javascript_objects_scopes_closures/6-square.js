ab659b826cbe202ee5de899b52607a68388e51f7
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-01 16:22:49 -0700

    Added instance method called charPrint(c) that prints the rectangle using the character c

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,70 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w === undefined || h === undefined || w <= 0 || h <= 0) {
+      // create empty object instance if any above conditions are met
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let row = 0;
+    let column;
+
+    while (row < this.height) {
+      let rowString = '';
+      column = 0;
+
+      while (column < this.width) {
+        rowString += 'X';
+        column++;
+      }
+      console.log(rowString);
+      row++;
+    }
+  }
+
+  rotate () {
+    const tempValue = this.height;
+    this.height = this.width;
+    this.width = tempValue;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    let row = 0;
+    let column;
+
+    while (row < this.size) {
+      let rowString = '';
+      column = 0;
+      while (column < this.size) {
+        rowString += c;
+        column++;
+      }
+      console.log(rowString);
+      row++;
+    }
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
