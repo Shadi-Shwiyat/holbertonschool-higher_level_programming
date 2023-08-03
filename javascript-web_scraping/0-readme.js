dcb421b5b051b021b15b9edbc1a441794c6f359c
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-03 11:42:33 -0700

    Script reads and prints contents of a file passed as an argument

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const filePath = args[0];
+
+fs.readFile(filePath, 'utf8', (err,data) => {
+    if (err) {
+        console.error('no such file or directory', err);
+    } else {
+        console.log(data);
+    }
+});
