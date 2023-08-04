1163554aa7e65a80d268c4a9fa532a30a7a021d3
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-04 12:04:01 -0700

    Script logs the number of times a specific character is present in all star wars movies

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const args = process.argv.slice(2);
+const url = args[0];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Failed to send request', error);
+  } else {
+    const jsonData = JSON.parse(body);
+    const charID = 'https://swapi-api.hbtn.io/api/people/18/';
+    let moviesPresent = 0;
+
+    for (const movie of jsonData.results) {
+      for (const characterUrl of movie.characters) {
+        if (characterUrl === charID) {
+          moviesPresent++;
+          break;
+        }
+      }
+    }
+    console.log(moviesPresent);
+  }
+});
