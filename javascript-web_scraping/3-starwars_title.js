bd8fefba46140b83709080a1f6b3015ed8e50699
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-04 11:32:31 -0700

    Script uses the starwars api to retrieve the title based on passed in ID

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,12 +3,15 @@
 const request = require('request');
 
 const args = process.argv.slice(2);
-const url = args[0];
+const id = args[0];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request.get(url, (error, response) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.error('Failed to send request', error);
   } else {
-    console.log('code: ', response.statusCode);
+    const jsonData = JSON.parse(body);
+    const title = jsonData.title;
+    console.log(title);
   }
 });
