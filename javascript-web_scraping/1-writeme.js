4459707ea524006622760834306a2f86132df934
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-04 10:53:09 -0700

    Script writes a string to a file using java filesystem

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -6,10 +6,8 @@ const args = process.argv.slice(2);
 const filePath = args[0];
 const string = args[1];
 
-fs.writeFile(filePath, args[1], 'utf8', (err, data) => {
+fs.writeFile(filePath, string, 'utf8', (err, data) => {
   if (err) {
     console.error('Error writing to file', err);
-  } else {
-    console.log('Data successfully written');
   }
 });
