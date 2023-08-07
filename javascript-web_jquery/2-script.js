382a82563c9f39367c425e8a3ddef6fbf2b9ecf1
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-07 16:49:16 -0700

    JavaScript script that updates the text color of the <header> element to red (#FF0000) using JQuery API

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 $(document).ready(function () {
-    // Add a click event listener to the element with ID "red_header"
     $("#red_header").click(function () {
-      // Change the color of the "red_header" element to red
       $(this).css("color", "red");
     });
 });
