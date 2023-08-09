7c1ab160eba59b3b3351f778720870a801a469e0
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-09 09:40:43 -0500

    JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json

--- javascript-web_jquery/9-script.js
+++ javascript-web_jquery/9-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 $(document).ready(function() {
-  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function(data) {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function(data) {
     $('#hello').text(data.hello);
   });
 });
