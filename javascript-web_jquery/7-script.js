2808aac0e0d26740c2393002e16db4eec310c026
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-08 17:56:53 -0700

    JavaScript script that fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.error('Failed to fetch api data', err)
+  } else {
+    const jsonData = JSON.parse(body);
+    const name = jsonData.name;
+
+    $(document).ready(function () {
+      $("#character").text(name);
+    });
+  }
+});
