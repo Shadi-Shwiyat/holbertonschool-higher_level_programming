37aeb74f175d1ab31e5c011abe21c9a92b8b3305
Shadi-Shwiyat <raz611103@gmail.com>
2023-08-08 18:30:53 -0700

    JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -3,10 +3,10 @@
 $(document).ready(function () {
   $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
     var movies = data.results;
-    var listMoviesDiv = $('#list_movies');
+    var listMoviesUl = $('#list_movies').append('<ul></ul>').find('ul');
 
     for (var i = 0; i < movies.length; i++) {
-      listMoviesDiv.append('<li>' + movies[i].title + '<li>');
+      listMoviesUl.append('<li>' + movies[i].title + '</li>');
     }
   });
 });
